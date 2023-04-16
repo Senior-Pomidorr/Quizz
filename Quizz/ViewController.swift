bb7188bac32cdc1ffceb8ea207952dfd8b0ddd12
«Daniil <danilkulikovskiy@gmail.com>
2023-04-16 23:07:58 +0300

    Initial Commit

---
+++ Quizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizz
+//
+//  Created by Daniil Kulikovskiy on 16.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
